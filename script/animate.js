69fb2f1f942c2d149fb797b2c107d16aa570dc2f
laurypintos13 <laurypintos.13@gmail.com>
2023-11-15 19:05:14 -0300

    add animations add sections

---
+++ script/animate.js
@@ -0,0 +1,34 @@
+
+window.sr = ScrollReveal();
+
+sr.reveal('.animateFooter', {
+    duration: 2000
+
+});
+
+sr.reveal('.animateHeader', {
+    duration: 2000
+
+});
+
+sr.reveal('.about', {
+    duration: 2000,
+    origin: 'left',
+    distance: '600px'
+
+
+});
+sr.reveal('.cardIndex', {
+    duration: 4000
+
+});
+
+sr.reveal('.form', {
+    duration: 2000,
+    origin: 'right',
+    distance: '600px'
+});
+sr.reveal('.pageform', {
+    duration: 2000,
+});
+
